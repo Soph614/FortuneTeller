b97904462055870efeedec3b6322a88ca525a364
soph <sophia@javamate.net>
2026-02-16 21:11:34 -0500

    Commit 2

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,62 +1,110 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Random;
+import java.awt.GraphicsEnvironment;
 
-public class FortuneTellerFrame extends JFrame{
-    JPanel mainPanel;
-    JPanel iconPanel = new JPanel();
-    JPanel displayPanel;
-    JPanel controlPanel;
+public class FortuneTellerFrame extends JFrame
+{
+    JPanel mainPnl;
+    JPanel iconPnl;  // Top
+    JPanel displayPnl; // Center
+    JPanel controlPnl; // Bottom
 
-    JTextArea displayTA;
-    JScrollPane scroller;
+    JTextArea displayTextArea;
+    JScrollPane scrollbar;
 
-    JLabel titleLabel = new JLabel();
+    JLabel titleLabel;
     ImageIcon icon;
 
     JButton readFortuneBtn;
     JButton quitBtn;
 
+    Random rnd = new Random();
+
+    ArrayList<String> fortunes = new ArrayList<>();
+
     public static void main (String[] args) {
-        new FortuneTellerFrame();
-    }
-    public FortuneTellerFrame() {
-        JPanel contentPanel = new JPanel();
-        JPanel buttonPanel = new JPanel();
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-
-        createTopPanel();
-        mainPanel.add(iconPanel, BorderLayout.NORTH);
-
-
-        createMiddlePanel();
-        createBottomPanel();
-        JPanel top = new JPanel();
-
-        JFrame ftFrame = new JFrame();
-
-        ftFrame.setTitle("Fortune Teller");
-        // MUST GIVE THESE FOR EACH JFRAME
-        ftFrame.setSize(400,400); // makes the width and height 400
-        ftFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // make the program finish when you close the window
-        ftFrame.setVisible(true); // make it visible
+            JFrame frame = new FortuneTellerFrame();
     }
 
-    private void createTopPanel() {
-        JPanel iconPanel = new JPanel();
-        ImageIcon icon = new ImageIcon("src/ftImage.jpg");
-        titleLabel.setLayout(new BorderLayout());
+    public FortuneTellerFrame()
+    {
+        fortunes.add("Death is in your future.");
+        fortunes.add("A large pepperoni pizza is awaiting you.");
+        fortunes.add("Your next roommate will be very unorganized.");
+        fortunes.add("You need to take a shower more often.");
+        fortunes.add("Your next best friend will listen without saying anything! Aside from a meow here and there.");
+        mainPnl = new JPanel();
+        mainPnl.setLayout(new BorderLayout());
+
+        createIconPanel();
+        mainPnl.add(iconPnl, BorderLayout.NORTH);
+
+        createDisplayPanel();
+        mainPnl.add(displayPnl, BorderLayout.CENTER);
+
+        createControlPanel();
+        mainPnl.add(controlPnl, BorderLayout.SOUTH);
+
+        add(mainPnl);
+        setTitle("Fortune Teller");
+        setSize(700, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    private void createIconPanel()
+    {
+        iconPnl = new JPanel();
+        icon = new ImageIcon("src/fortuneTellerImage.jpg");
+
+        // HAVE TO RESIZE THE IMAGE
+        // Got idea from trolologuy, https://stackoverflow.com/a/18335435 on 02/16/2026 License - CC BY-SA 3.0
+        Image tooBigImage = icon.getImage();
+        Image smallerImage = tooBigImage.getScaledInstance(300, 300, Image.SCALE_DEFAULT);
+        icon = new ImageIcon(smallerImage);
+        // resizing complete
+
         titleLabel = new JLabel("Fortune Teller", icon, JLabel.CENTER);
+        titleLabel.setFont(new Font("Chalkduster", Font.PLAIN, 34));
+        // Obscure code to align the text to the Icon!
         titleLabel.setVerticalTextPosition(JLabel.BOTTOM);
         titleLabel.setHorizontalTextPosition(JLabel.CENTER);
 
-        iconPanel.add(titleLabel);
+        iconPnl.add(titleLabel);
     }
 
-    private void createMiddlePanel() {
+    private void createDisplayPanel()
+    {
+        displayPnl = new JPanel();
+        displayTextArea = new JTextArea(10, 50);
+        displayTextArea.setEditable(false);
+        displayTextArea.setFont(new Font("Herculanum", Font.PLAIN, 12));
+        scrollbar = new JScrollPane(displayTextArea);
+        displayPnl.add(scrollbar);
     }
 
-    private void createBottomPanel() {
+
+    private void createControlPanel()
+    {
+        controlPnl = new JPanel();
+        controlPnl.setLayout(new GridLayout(1, 2));
+
+        readFortuneBtn = new JButton("Read My Fortune!");
+        readFortuneBtn.addActionListener((ActionEvent ae) ->
+        {
+            int index = rnd.nextInt(fortunes.size());
+            String fortune = fortunes.get(index);
+            displayTextArea.append(fortune +"\n");
+        });
+
+        quitBtn = new JButton("Quit");
+        quitBtn.addActionListener((ActionEvent ae) -> System.exit(0));
+
+        controlPnl.add(readFortuneBtn);
+        controlPnl.add(quitBtn);
+
     }
 }
